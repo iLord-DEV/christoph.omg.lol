dbe7b6521d02f35b6aac573cb62eaeeec51090cf
Christoph <post@christoph-heim.de>
2022-11-30 14:52:12 +0100

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,123 @@
+const EleventyVitePlugin = require('@11ty/eleventy-plugin-vite')
+const path = require('path')
+const Image = require('@11ty/eleventy-img')
+
+// IMAGE SHORTCODE
+async function imageShortcode(src, alt, sizes) {
+  const options = {
+    useCache: true, // set to false while you are playing with the img quality
+    widths: [300, 600, 1200],
+    formats: ['avif', 'webp', 'jpg'],
+    outputDir: 'dist/img',
+    sharpWebpOptions: {
+    	quality: 85,
+    	smartSubsample: true,
+    	effort: 6
+    },
+    sharpJpegOptions: {
+    	quality: 85
+    },
+    sharpAvifOptions: {
+    	quality: 85
+    },
+
+    filenameFormat: function (id, src, width, format, options) {
+      const extension = path.extname(src)
+      const name = path.basename(src, extension)
+
+      return `${name}-${width}w.${format}`
+    }
+  }
+
+  const metadata = await Image(src, options)
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: 'lazy',
+    decoding: 'async'
+  }
+
+  return Image.generateHTML(metadata, imageAttributes, {
+    whitespaceMode: 'inline'
+  })
+}
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(EleventyVitePlugin, {
+    viteOptions: {
+      clearScreen: false,
+
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: `$injectedColor: orange;`,
+            devSourcemap: true
+          }
+        }
+      },
+
+      server: {
+        mode: 'development',
+        middlewareMode: true
+      },
+      appType: 'custom',
+      build: {
+        mode: 'production',
+        rollupOptions: {
+          output: {
+            chunkFileNames: 'js/[name]-[hash].js',
+            entryFileNames: 'js/[name]-[hash].js',
+
+            assetFileNames: ({ name }) => {
+              if (/\.(avif|webp|gif|jpe?g|png|svg)$/.test(name ?? '')) {
+                return 'img/[name]-[hash][extname]'
+              }
+
+              if (/\.(woff|woff2|otf)$/.test(name ?? '')) {
+                return 'fonts/[name]-[hash][extname]'
+              }
+
+              if (/\.css$/.test(name ?? '')) {
+                return 'css/[name]-[hash][extname]'
+              }
+
+              // default value
+              // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+              return '[name]-[hash][extname]'
+            }
+          }
+        }
+      }
+    }
+  })
+
+  eleventyConfig.setServerOptions({
+  showAllHosts:true,}),
+
+  // Passthrough copy
+  eleventyConfig.addPassthroughCopy('src/css')
+  eleventyConfig.addPassthroughCopy('src/js')
+  eleventyConfig.addPassthroughCopy('src/img')
+  eleventyConfig.addPassthroughCopy('src/public')
+  eleventyConfig.setServerPassthroughCopyBehavior('copy')
+
+  // image shortcodes
+  eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode)
+  eleventyConfig.addLiquidShortcode('image', imageShortcode)
+  eleventyConfig.addJavaScriptFunction('image', imageShortcode)
+
+  return {
+    templateFormats: ['md', 'njk', 'html', 'liquid'],
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+
+    dir: {
+      input: 'src',
+      output: 'dist',
+      includes: '_includes',
+      // layouts: '_layouts',
+      // data: '_data'
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,71 @@
+# Lords Eleventy Starter 🚀
+
+A simple starting point for fast Websites :-)
+
+## Features
+
+* Eleventy 2.0
+* Vite
+* PostCSS
+* Image Optimazation
+
+## Getting started
+
+Start by [generating a new repository based on this project](https://github.com/iLord-DEV/lords-eleventy-vite-starter/generate).
+
+Make sure you are using Node >= v16!
+
+```sh
+node -v
+```
+
+If your Version is under 16, please update or install a version manager [like nvm](https://github.com/nvm-sh/nvm)
+
+After cloning (or downloading) the repository to your local machine, install all dependencies with the command
+
+```sh
+npm install
+```
+
+## Run dev server
+
+The project comes with Eleventy’s built-in development server. You can start the server with
+
+```sh
+npm start
+````
+
+or
+
+```sh
+npx @11ty/eleventy --serve
+````
+
+## Build
+
+To trigger a production build, use
+
+```sh
+npm run build
+````
+
+or
+
+```sh
+npx @11ty/eleventy
+```
+
+## CSS
+
+Sass or PostCSS 
+
+CSS methodology: CUBE CSS (give it a try or use your own) [https://cube.fyi](https://cube.fyi)
+
+[Autoprefixer](https://github.com/postcss/autoprefixer) adds necessary browser prefixes. The [browserslist](https://github.com/browserslist/browserslist) settings can be adjusted in `package.json`.
+
+## Images
+
+Images are optimized and inserted via shortcode to your template files.
+
+##
+
